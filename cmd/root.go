46733ae0e64619acb677a35b2e6115ed257df4d2
gidyon <gideonhacer@gmail.com>
2019-03-08 13:06:46 +0300

    file server

--- cmd/root.go
+++ cmd/root.go
@@ -16,28 +16,32 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cobra",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "file-server",
+	Short: "A file server for web files",
+	Long:  `A static file server. Serves html, javascript, css, icons, fonts and image files`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		port, err := cmd.Flags().GetString("port")
+		if err != nil {
+			return err
+		}
+		dir, err := cmd.Flags().GetString("dir")
+		if err != nil {
+			return err
+		}
+		log.Printf("Http file server started on port: %s\n", port)
+		return http.ListenAndServe(port, http.FileServer(http.Dir(dir)))
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -50,40 +54,7 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cobra")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	rootCmd.Flags().StringP("port", "p", ":9090", "Port to serve files")
+	rootCmd.Flags().StringP("dir", "d", "", "Directory to serve files")
+	rootCmd.Flags().BoolP("tls", "tls", false, "Encrypt communication with tls")
 }
